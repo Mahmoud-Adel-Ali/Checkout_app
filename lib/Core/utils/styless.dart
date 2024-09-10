73775c0d22bf036ab58f01bbf2d4c53779295f1a
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-09 17:28:07 -0700

    create app styles

---
+++ lib/Core/utils/styless.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+abstract class Styless {
+  static const TextStyle style25 = TextStyle(
+    fontSize: 25.0,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+    fontFamily: 'Inter',
+  );
+  static const TextStyle style24 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+    fontFamily: 'Inter',
+  );
+  static TextStyle style20 = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.w600,
+    color: Colors.black.withOpacity(0.8),
+    fontFamily: 'Inter',
+  );
+  static const TextStyle style18 = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+    fontFamily: 'Inter',
+  );
+  static const TextStyle styleBold18 = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+    fontFamily: 'Inter',
+  );
+}
