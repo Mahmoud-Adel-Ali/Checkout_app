c5dd23c84f5c777fce7594b6ed62ad32c2fa6dfc
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-10 17:26:09 -0700

    add payment images

--- lib/Core/utils/images.dart
+++ lib/Core/utils/images.dart
@@ -2,8 +2,14 @@
 class AppImages {
   AppImages._();
   
+  static const String imagesApplePay = "assets/images/apple_pay.svg";
+
   static const String imagesArrowBack = "assets/images/arrow_back.svg";
 
   static const String imagesBasketImage = "assets/images/basket_image.png";
+
+  static const String imagesCard = "assets/images/card.svg";
+
+  static const String imagesPaypal = "assets/images/paypal.svg";
 }
 
--- lib/Features/Checkout/Presentation/Views/payment_details_view.dart
+++ lib/Features/Checkout/Presentation/Views/payment_details_view.dart
@@ -1,4 +1,5 @@
 import 'package:checkout_app/Core/utils/functions/build_app_bar.dart';
+import 'package:checkout_app/Features/Checkout/Presentation/Views/Widgets/payment_details_view_body.dart';
 import 'package:flutter/material.dart';
 
 class PaymentDetailsView extends StatelessWidget {
@@ -12,6 +13,7 @@ class PaymentDetailsView extends StatelessWidget {
           onTapOnLeading: () {
             Navigator.pop(context);
           }),
+      body: const PaymentDetailsViewBody(),
     );
   }
 }
