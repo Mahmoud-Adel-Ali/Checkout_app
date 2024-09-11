39aa9bba3036887043e9d2a9d6bb0db210d2bf09
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-10 17:16:16 -0700

    create payment details view and refactor build app bar

---
+++ lib/Core/utils/functions/build_app_bar.dart
@@ -0,0 +1,19 @@
+import 'package:checkout_app/Core/utils/images.dart';
+import 'package:checkout_app/Core/utils/styless.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+AppBar buildAppBart({required String title, void Function()? onTapOnLeading}) {
+  return AppBar(
+    title: Text(title, style: Styless.style25),
+    centerTitle: true,
+    leading: InkWell(
+        onTap: onTapOnLeading,
+        child: Center(child: SvgPicture.asset(AppImages.imagesArrowBack))),
+    backgroundColor: Colors.white,
+    elevation: 0.0,
+    flexibleSpace: FlexibleSpaceBar(
+      background: Container(color: Colors.white),
+    ),
+  );
+}
--- lib/Features/Checkout/Presentation/Views/my_cart_view.dart
+++ lib/Features/Checkout/Presentation/Views/my_cart_view.dart
@@ -1,8 +1,6 @@
-import 'package:checkout_app/Core/utils/images.dart';
-import 'package:checkout_app/Core/utils/styless.dart';
+import 'package:checkout_app/Core/utils/functions/build_app_bar.dart';
 import 'package:checkout_app/Features/Checkout/Presentation/Views/Widgets/my_cart_view_body.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
@@ -10,16 +8,7 @@ class MyCartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Cart', style: Styless.style25),
-        centerTitle: true,
-        leading: Center(child: SvgPicture.asset(AppImages.imagesArrowBack)),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        flexibleSpace: FlexibleSpaceBar(
-          background: Container(color: Colors.white),
-        ),
-      ),
+      appBar: buildAppBart(title: 'My Cart'),
       body: const MyCartViewBody(),
     );
   }
