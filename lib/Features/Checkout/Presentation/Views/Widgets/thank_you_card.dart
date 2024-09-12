f8f30fa74343ca1148b34a89a002d69847ffcbcb
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-12 15:25:20 -0700

    create payment item info

--- lib/Features/Checkout/Presentation/Views/Widgets/thank_you_card.dart
+++ lib/Features/Checkout/Presentation/Views/Widgets/thank_you_card.dart
@@ -1,5 +1,8 @@
-
+import 'package:checkout_app/Core/utils/styless.dart';
+import 'package:checkout_app/Features/Checkout/Presentation/Views/Widgets/order_info_item.dart';
+import 'package:checkout_app/Features/Checkout/Presentation/Views/Widgets/total_price.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ThankYouCard extends StatelessWidget {
   const ThankYouCard({
@@ -8,12 +11,22 @@ class ThankYouCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: ShapeDecoration(
-        color: const Color(0xffD9D9D9),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
-        ),
+    return Padding(
+      padding: const EdgeInsets.only(top: 66.0),
+      child: Column(
+        children: [
+          const Text('Thank you!', style: Styless.style25),
+          Text('Your transaction was successful', style: Styless.style20),
+          const SizedBox(height: 42),
+          const OrderInfoItem(orderName: 'Date', orderprice: '01/24/2023'),
+          const SizedBox(height: 20),
+          const OrderInfoItem(orderName: 'Time', orderprice: '10:15 AM'),
+          const SizedBox(height: 20),
+          const OrderInfoItem(orderName: 'To', orderprice: 'Sam Louis'),
+          const Divider(height: 60, thickness: 2),
+          const TotalPrice(totalPrice: r'$50.97'),
+          const SizedBox(height: 30),
+        ],
       ),
     );
   }
