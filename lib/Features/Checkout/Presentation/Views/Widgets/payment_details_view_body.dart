de6c5f0edd418d8fca4e884dc84cbeea89fab24c
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-12 13:41:13 -0700

    create thank you view

--- lib/Features/Checkout/Presentation/Views/Widgets/payment_details_view_body.dart
+++ lib/Features/Checkout/Presentation/Views/Widgets/payment_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:checkout_app/Core/Widgets/custom_buttom.dart';
 import 'package:checkout_app/Features/Checkout/Presentation/Views/Widgets/custom_credit_card.dart';
 import 'package:checkout_app/Features/Checkout/Presentation/Views/Widgets/payment_methods_list_view.dart';
+import 'package:checkout_app/Features/Checkout/Presentation/Views/thank_you_view.dart';
 import 'package:flutter/material.dart';
 
 class PaymentDetailsViewBody extends StatefulWidget {
@@ -34,6 +35,8 @@ class _PaymentDetailsViewBodyState extends State<PaymentDetailsViewBody> {
                 const Expanded(child: SizedBox(height: 25)),
                 CustomButtom(
                     onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const ThankYouView()));
                       if (formKey.currentState!.validate()) {
                         formKey.currentState!.save();
                       } else {
