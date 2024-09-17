b0dc6a1f4618b3f2acd30fecb03056d5c6ef1962
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-17 16:45:58 -0700

    implement payment intent

--- lib/Core/utils/stripe_service.dart
+++ lib/Core/utils/stripe_service.dart
@@ -1,3 +1,20 @@
+import 'package:checkout_app/Core/utils/api_keys.dart';
+import 'package:checkout_app/Core/utils/api_service.dart';
+import 'package:checkout_app/Features/Checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_app/Features/Checkout/data/models/payment_intent_model/payment_intent_model.dart';
+
 class StripeService {
-  
+  final ApiService api = ApiService();
+
+  Future<PaymentIntentModel> createPaymentIntent(
+      PaymentIntentInputModel paymentIntentInputModel) async {
+    var response = await api.post(
+      body: paymentIntentInputModel.toJson(),
+      token: ApiKeys.secretKey,
+      url: 'https://api.stripe.com/v1/payment_intents',
+    );
+
+    var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+    return paymentIntentModel;
+  }
 }
