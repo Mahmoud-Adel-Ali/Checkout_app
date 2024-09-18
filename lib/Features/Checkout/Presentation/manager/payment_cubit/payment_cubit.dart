16fa88b0ca8dbdcd72ae77bb8d56ddb9a528761b
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-18 16:35:09 -0700

    create payment cubit

---
+++ lib/Features/Checkout/Presentation/manager/payment_cubit/payment_cubit.dart
@@ -0,0 +1,30 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:checkout_app/Features/Checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_app/Features/Checkout/data/repos/checkout_repo.dart';
+
+part 'payment_state.dart';
+
+class PaymentCubit extends Cubit<PaymentState> {
+  PaymentCubit({required this.checkoutRepo}) : super(PaymentInitial());
+
+  final CheckoutRepo checkoutRepo;
+
+  Future makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) async {
+    emit(PaymentLoading());
+    var data = await checkoutRepo.makePayment(
+        paymentIntentInputModel: paymentIntentInputModel);
+    data.fold(
+      (failure) => emit(PaymentFailure(errorMessage: failure.errorMessage)),
+      (r) => emit(PaymentSuccess()),
+    );
+  }
+
+  @override
+  void onChange(Change<PaymentState> change) {
+    log(change.toString());
+    super.onChange(change);
+  }
+}
