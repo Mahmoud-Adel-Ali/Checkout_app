dcfa851865ba1a0282a10dd9822412805867eafb
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-18 16:15:28 -0700

    create checkout repo

---
+++ lib/Features/Checkout/data/repos/checkout_repo.dart
@@ -0,0 +1,12 @@
+import 'package:checkout_app/Core/errors/failures.dart';
+import 'package:checkout_app/Features/Checkout/data/models/payment_intent_input_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class CheckoutRepo {
+  Future<Either<Failure, void>> makePayment(
+      {required PaymentIntentInputModel paymentIntentInputModel}) {
+    // Implementation for making payment using paymentIntentInputModel
+    // Replace with actual implementation
+    throw ServerFailure(errorMessage: 'Payment failed');
+  }
+}
