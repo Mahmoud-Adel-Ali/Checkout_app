f02740f9b4c6e64b9ed6eb5bb4c7926536a58957
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-09-21 19:11:25 -0700

    create Init Payment Sheet Input Model

---
+++ lib/Features/Checkout/data/models/inti_payment_sheet_input_model.dart
@@ -0,0 +1,11 @@
+class InitPaymentSheetInputModel {
+  final String customerId;
+  final String clientSecret;
+  final String ephemeralKeySecret;
+
+  InitPaymentSheetInputModel({
+    required this.customerId,
+    required this.clientSecret,
+    required this.ephemeralKeySecret,
+  });
+}
