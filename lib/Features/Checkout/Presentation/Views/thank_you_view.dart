1d41291cac75f812bc2e84a7ddf931c71bb33a64
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-16 21:59:52 -0700

    handel thank you view

--- lib/Features/Checkout/Presentation/Views/thank_you_view.dart
+++ lib/Features/Checkout/Presentation/Views/thank_you_view.dart
@@ -1,9 +1,33 @@
 import 'package:checkout_app/Core/utils/functions/build_app_bar.dart';
 import 'package:checkout_app/Features/Checkout/Presentation/Views/Widgets/thank_you_view_body.dart';
+import 'package:checkout_app/Features/Checkout/Presentation/Views/my_cart_view.dart';
 import 'package:flutter/material.dart';
 
-class ThankYouView extends StatelessWidget {
+class ThankYouView extends StatefulWidget {
   const ThankYouView({super.key});
+
+  @override
+  State<ThankYouView> createState() => _ThankYouViewState();
+}
+
+class _ThankYouViewState extends State<ThankYouView> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 5),
+      () {
+        Navigator.pushAndRemoveUntil(
+          // ignore: use_build_context_synchronously
+          context,
+          MaterialPageRoute(builder: (context) => const MyCartView()),
+          (route) {
+            return false;
+          },
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
